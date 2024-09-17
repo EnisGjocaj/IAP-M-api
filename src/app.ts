5bea671105c99143c5d2dddd7d6bda34f1de7ffa
Enis Gjocaj <97288173+EnisGjocaj@users.noreply.github.com>
2024-09-17 15:51:44 +0200

    cached faster api

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,12 @@ import cors from "cors";
 import fs from 'fs';
 
 import multer from "multer";
+import NodeCache from 'node-cache';
 
+import { TeamMemberService } from './apps/teamMember/teamMember.service';
+
+const cache = new NodeCache({ stdTTL: 60 * 5 });
+const teamMemberService = new TeamMemberService(); 
 
 const app = express();
 
@@ -24,6 +29,27 @@ app.use(cors({
   origin: 'https://iap-m.com', // Allow requests from this domain
 
 }));
+
+
+
+
+
+async function preloadCache() {
+  try {
+    const teamMembersData = await teamMemberService.getAllTeamMembers(); // Fetch team members
+    cache.set('/team-members', teamMembersData.message); // Cache the data
+    console.log("Team members data preloaded into cache.");
+  } catch (error) {
+    console.error("Error preloading team members:", error);
+  }
+}
+
+// Call preload function on server startup
+preloadCache();
+
+// Refresh the cache every 5 minutes
+setInterval(preloadCache, 5 * 60 * 1000);
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -51,13 +77,30 @@ const storage = multer.diskStorage({
   // app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
   app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
-
+  function cacheMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const key = req.originalUrl; // use the request URL as the cache key
+    const cachedResponse = cache.get(key);
+  
+    if (cachedResponse) {
+      return res.json(cachedResponse); // Return the cached response if available
+    } else {
+      const originalJson = res.json.bind(res); // Store the original res.json method
+  
+      res.json = (body: any) => {
+        cache.set(key, body); // Cache the response
+        return originalJson(body); // Ensure the overridden method returns the original response
+      };
+  
+      next(); // Continue to the next middleware if no cached response
+    }
+  }
+  
 // Register the routes
-app.use('/news', newsRouter);
-app.use('/applications', applicationRouter);
-app.use("/team-members", teamMemberRouter);
-app.use("/users", userRouter);
-app.use("/manageUsers", manageUserRouter);
-app.use("/dashboard", dashboardRouter);
+app.use('/news', cacheMiddleware, newsRouter);
+app.use('/applications', cacheMiddleware, applicationRouter);
+app.use("/team-members", cacheMiddleware, teamMemberRouter);
+app.use("/users", cacheMiddleware, userRouter);
+app.use("/manageUsers", cacheMiddleware,  manageUserRouter);
+app.use("/dashboard", cacheMiddleware, dashboardRouter);
 
 export default app;
